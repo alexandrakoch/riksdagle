fd4e3a087163b9447f07e094b815bff811e0a210
Alexandra Koch <alexandra@alexandrakoch.se>
2025-07-27 17:01:20 +0200

    Share button

--- script.js
+++ script.js
@@ -16,14 +16,17 @@ var partyMap = {
 const submitButton = document.getElementById("submit")
 const resultsDialog = document.getElementById("resultsModal")
 const closeModalButton = document.getElementById("closeModal")
+const shareButton = document.getElementById("shareButton")
 
 //get variable elements 
 const resultsMessage = document.getElementById("resultsMessage")
 const resultsEmoji = document.getElementById("resultsEmoji")
 
+const currentDate = new Date();
 var guesses = 0;
 var partyAnswer;
 var guessesResult = ["⬜", "⬜", "⬜", "⬜"]
+let result;
 function daysIntoYear(date) {
     return (Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) - Date.UTC(date.getFullYear(), 0, 0)) / 24 / 60 / 60 / 1000;
 }
@@ -35,7 +38,7 @@ async function fillData() {
             throw new Error(`Response status: ${response.status}`);
         }
         const json = await response.json();
-        let dayOfYear = daysIntoYear(new Date());
+        let dayOfYear = daysIntoYear(currentDate);
         partyAnswer = json[dayOfYear].party
         document.getElementById("portrait").src = json[dayOfYear].image_url;
     } catch (error) {
@@ -58,6 +61,7 @@ function formCallback(event) {
         guessesResult[guesses] = "🟩";
         submitButton.disabled = true;
         party.disabled = true;
+        result = true;
         alert("Rätt svarat! " + guessesResult.join(""));
         resultsMessage.innerText = "Snyggt jobbat!"
         resultsEmoji.innerText = guessesResult.join("")
@@ -74,6 +78,7 @@ function formCallback(event) {
     if (guesses == 3 & guessedPartyId != partyAnswer) {
         submitButton.disabled = true;
         party.disabled = true;
+        result = false;
         resultsMessage.innerText = "Pyramidalt klumpigt!"
         resultsEmoji.innerText = guessesResult.join("")
         resultsDialog.showModal()
@@ -81,8 +86,17 @@ function formCallback(event) {
     guesses++;
     party.value = "";
 }
+
+function shareStringBuilder(result, guessArray) {
+    let beginString = "Riksdagle " + currentDate.toISOString().substring(0, 10);
+    const endString = "https://alexandrakoch.github.io/riksdagle/"
+    return beginString + "\r\n" + guessArray.join("") + "\r\n" + endString
+}
 fillData();
 form.addEventListener("submit", formCallback, false);
 closeModalButton.addEventListener("click", () => {
     resultsDialog.close();
 });
+shareButton.addEventListener("click", () => {
+    navigator.clipboard.writeText(shareStringBuilder(result, guessesResult))
+})
